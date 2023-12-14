cf193e93f5f87562d7b2c635c898e5c7c3c28d0b
Chethan P <125786242+chethanp1234@users.noreply.github.com>
2023-12-14 20:53:16 +0530

    Add files via upload
    Hello there !!
    This is a simple web based music player built using basic HTML , CSS and JS :)

---
+++ PlayList.js
@@ -0,0 +1,19 @@
+let songs = [
+    {
+        
+        audio : "m1"
+    },
+    {
+        
+        audio : "m2"
+    },
+    {
+        
+        audio : "m3"
+    },
+    {
+        
+        audio : "m4"
+    },
+
+]
